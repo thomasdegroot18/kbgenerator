4c93f4f333f4504cc354721d2f0d784231a29b30
Thomas de Groot <thomas.de.groot@triply.cc>
2021-06-07 17:15:00 +0200

    Added in presentation for ESWC.

---
+++ docs/shared/js/presentation.js
@@ -0,0 +1,39 @@
+function getQuery(catalogUrl, cb) {
+  var request = new XMLHttpRequest();
+  request.open('GET', catalogUrl, true);
+  request.setRequestHeader('Accept', 'application/json')
+
+  request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+      // Success!
+      try {
+        cb(null,JSON.parse(this.response));
+      } catch(e) {
+        cb(e);
+      }
+    } else {
+      cb(new Error('Could not fetch query information from ' + catalogUrl))
+    }
+  };
+
+  request.onerror = function() {
+    cb(new Error('Request error when fetching query information from ' + catalogUrl))
+  };
+
+  request.send();
+}
+function populateQueriesFromQueryCatalogue() {
+  var numDone = 0;
+  var codeItems = document.querySelectorAll('code[data-config-ref]')
+  codeItems.forEach(function(el) {
+    var url = el.getAttribute("data-config-ref");
+    getQuery(url, function(err, json) {
+      numDone++;
+      if (err) return console.error(err);
+      el.innerHTML = Prism.highlight(json.requestConfig.payload.query, Prism.languages.sparql, 'sparql');
+    })
+  })
+}
+window.addEventListener('load', function() {
+    populateQueriesFromQueryCatalogue()
+})
